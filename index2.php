08dff04c7b802125a7ae2c60302ce3c04533509a
amedeopro <amedeopro@me.com>
2019-01-21 22:50:45 +0100

    esercizio terminato

--- index2.php
+++ index2.php
@@ -5,6 +5,15 @@
     <title></title>
   </head>
   <body>
+    <?php
 
+    $parola = $_GET['parola'];
+
+    $paragrafo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+    $paragraMod = str_replace($parola, '***', $paragrafo);
+
+    echo $paragraMod;
+     ?>
   </body>
 </html>
